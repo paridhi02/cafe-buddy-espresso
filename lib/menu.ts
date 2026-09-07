df7a70064aedbe2832034a4f11a802204ad3c1f5
paridhi02 <85054653+paridhi02@users.noreply.github.com>
2026-09-07 16:56:00 +0530

    Add menu compatibility types

---
+++ lib/menu.ts
@@ -0,0 +1,3 @@
+export type MenuItem={id:number;category:string;name:string;description:string;price:number;prepMinutes:number;vegetarian:boolean;available:boolean};
+export const menu:MenuItem[]=[];
+export const categories=['All'];
